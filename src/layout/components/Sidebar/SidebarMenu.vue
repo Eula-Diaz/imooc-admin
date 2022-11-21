f8921f49106e5f3868ce4a87052d5929e3782758
muzi <xiumuds@163.com>
2022-11-21 20:11:06 +0800

    feat: 解析路由，获取结构化数据

---
+++ src/layout/components/Sidebar/SidebarMenu.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-menu
+    :unique-opened="true"
+    default-active="2"
+    background-color="#545c64"
+    text-color="#fff"
+    active-text-color="#ffd04b"
+  >
+    <el-submenu index="1">
+      <template #title>
+        <i class="el-icon-location" />
+        <span>导航一</span>
+      </template>
+      <el-menu-item index="1-1">选项1</el-menu-item>
+      <el-menu-item index="1-2">选项2</el-menu-item>
+    </el-submenu>
+    <el-menu-item index="4">
+      <i class="el-icon-setting"></i>
+      <template #title>导航四</template>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { useRouter } from 'vue-router'
+import { filterRoutes, generateMenus } from '@/utils/route'
+
+const router = useRouter()
+const routes = computed(() => {
+  const routes = filterRoutes(router.getRoutes())
+  return generateMenus(routes)
+})
+console.log(routes)
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/utils/route.js
@@ -0,0 +1,69 @@
+import path from 'path'
+
+/**
+ * 返回所有子路由
+ */
+const getChildrenRoutes = (routes) => {
+  const result = []
+  routes.forEach((route) => {
+    if (route.children && route.children.length > 0) {
+      result.push(...route.children)
+    }
+  })
+}
+
+/**
+ * 处理脱离层级的路由：某个一级路由为其他子路由，则剔除该一级路由，保留路由层级
+ */
+export const filterRoutes = (routes) => {
+  const childrenRoutes = getChildrenRoutes(routes)
+  return routes.filter((route) => {
+    return !childrenRoutes.find((childrenRoute) => {
+      return childrenRoute.path === route.path
+    })
+  })
+}
+
+/**
+ * 判断数据是否为空
+ */
+function isNull(data) {
+  if (!data) return true
+  if (JSON.stringify(data) === '{}') return true
+  if (JSON.stringify(data) === '[]') return true
+  return false
+}
+
+/**
+ * 根据 routes 数据，返回对应 menu 规则数组
+ */
+export function generateMenus(routes, basePath = '') {
+  const result = []
+  routes.forEach((item) => {
+    // 不存在 children && 不存在 meta 直接返回
+    if (isNull(item.meta) && isNull(item.children)) return
+    // 存在 children 不存在 meta，进入迭代
+    if (isNull(item.meta) && !isNull(item.children)) {
+      result.push(...generateMenus(item.children))
+      return
+    }
+    // 合并 path 作为跳转路径
+    const routePath = path.resolve(basePath, item.path)
+    // 路由分离之后，存在同名父路由的情况，需要单独处理
+    let route = result.find((item) => item.path === routePath)
+    if (!route) {
+      route = {
+        ...item,
+        path: routePath,
+        children: []
+      }
+      if (route.meta.icon && route.meta.title) {
+        result.push(route)
+      }
+    }
+    if (item.children) {
+      route.children.push(...generateMenus(item.children, route.path))
+    }
+  })
+  return result
+}
