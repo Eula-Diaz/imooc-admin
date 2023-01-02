122efdb3aa1c5186156e723b4c48383b767097c8
muzi <xiumuds@163.com>
2023-01-02 20:57:26 +0800

    feat: 创建 PanThumb 头像组件

--- src/views/profile/components/ProjectCard.vue
+++ src/views/profile/components/ProjectCard.vue
@@ -1,7 +1,18 @@
 <template>
-  <div></div>
+  <div>
+    <pan-thumb
+      :image="$store.getters.userInfo.avatar"
+      :height="'100px'"
+      :width="'100px'"
+      :hoverable="false"
+    >
+      <div>Hello</div>
+      {{ $store.getters.userInfo.title }}
+    </pan-thumb>
+  </div>
 </template>
 <script setup>
 import {} from 'vue'
+import PanThumb from '@/components/PanThumb'
 </script>
 <style lang="scss" scoped></style>
