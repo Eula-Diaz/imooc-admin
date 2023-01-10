22dcaa21f54e861fcb2299b97a872f9dc6d7e06e
muzi <xiumuds@163.com>
2023-01-10 19:35:46 +0800

    feat: 全局属性处理时间展示问题

--- src/main.js
+++ src/main.js
@@ -7,8 +7,10 @@ import installIcons from '@/icons'
 import './styles/index.scss'
 import './permission'
 import i18n from './i18n'
+import installFilter from '@/filters'
 
 const app = createApp(App)
 installElementPlus(app)
 installIcons(app)
+installFilter(app)
 app.use(store).use(router).use(i18n).mount('#app')
